bef533fc9008e1a1fb05b4fc2c5a8f36a3502acb
Apan <apan0608@hotmail.com>
2019-08-03 17:35:26 +1000

    Impleted Dequeue using Linked List and Iterator, added unit tests for Deuque class

--- Queues/src/com/company/Deque.java
+++ Queues/src/com/company/Deque.java
@@ -1,36 +1,132 @@
 package com.company;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+/*
+ This class is implemented using linked list
+ */
 public class Deque<Item> implements Iterable<Item> {
 
-    public Deque() {
-
-    }
+    private Node first = null;
+    private Node last = null;
+    private int size = 0; // not sure if I wanted this
 
     public boolean isEmpty() {
-        return false;
+        return first == null;
     }
 
     public int size() {
-        return 0;
-    }
+        return size;
+    } // another way is to loop through it
 
+
+    // todo test how java reference type works
     public void addFirst(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("Item cannot be null");
 
+        Node newFirst = new Node(item);
+        newFirst.next = first;
+        first = newFirst;
+        if (size == 0) {
+            // first is the last
+            last = first;
+        }
+        size++;
     }
 
-    public void addLast(Item item) {}
+    public void addLast(Item item) {
+        if (item == null)
+            throw new IllegalArgumentException("Item cannot be null");
+        Node newNode = new Node(item);
+
+        if (size == 0) {
+            last = newNode;
+            first = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        size++;
+    }
 
     public Item removeFirst() {
+        if (first == null)
+            throw new NoSuchElementException("Item does not exist");
 
+        Item oriFirstValue = first.item;
+
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            Node second  = first.next;
+            first = second;
+        }
+        size--;
+        return oriFirstValue;
     }
 
-    public Item removeLast() {}
+    public Item removeLast() {
+        if (first == null)
+            throw new NoSuchElementException("Item does not exist");
 
-    public Iterator<Item> iterator() {}
+        Item lastValue = (Item) last.item;
+        last = null;
+        if (size == 1) {
+            first = null;
+        } else {
+            Node node = first;
+            while (node.next != null) {
+                // this is the second last, make it the last
+                if (node.next.next == null) {
+                    node.next = null;
+                    last = node;
+                    break;
+                }
+                node = node.next;
+            }
+        }
+        size--;
+        return lastValue;
+    }
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
 
     public static void main(String[] args) {
 
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("Remove is not supported in DequeIterator");
+        }
+
+        public Item next() {
+            if (current.next == null)
+                throw new NoSuchElementException("No more element in the collection");
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    private class Node {
+        Item item;
+        Node next = null;
+        // using previous will enhance the performance of remove last, but will make the implementation too complex
+
+        public Node(Item item) {
+            this.item = item;
+        }
     }
 
 }
--- Queues/src/com/company/TestDeque.java
+++ Queues/src/com/company/TestDeque.java
@@ -1,38 +1,188 @@
 package com.company;
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 
 public class TestDeque {
 
     @Test
     public void testIsEmpty() {
-
+        Deque deque = new Deque();
+        assertTrue(deque.isEmpty());
+        deque.addFirst(1);
+        assertFalse(deque.isEmpty());
     }
 
     @Test
     public void testSize() {
+        Deque<String> deque = new Deque<String>();
+
+        deque.addFirst("s");
+        deque.addFirst("s");
+        deque.addLast("s");
+        assertEquals(3, deque.size());
+        deque.removeLast();
+        assertEquals(2, deque.size());
+        deque.addLast("s");
+        deque.removeLast();
+        deque.removeFirst();
+        assertEquals(1, deque.size());
+        deque.removeFirst();
+        assertEquals(0, deque.size());
     }
 
     @Test
     public void testAddFirst() {
+        Deque<Integer> deque = new Deque<Integer>();
+        deque.addFirst(1);
+        Integer expectedFirst = 1;
 
+        int count = 0;
+        for (Integer item: deque) {
+            if (count == 0)
+                assertEquals(expectedFirst, item);
+        }
+
+        deque.addFirst(2);
+        expectedFirst = 2;
+        for (Integer item: deque) {
+            if (count == 0)
+                assertEquals(expectedFirst, item);
+            if (count == 1)
+                assertEquals((Integer)1, item);
+            count++;
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddFirst_ParameterNull_Exception() {
+        Deque deque = new Deque();
+        deque.addFirst(null);
     }
 
     @Test
-    public void testAddLast() {}
+    public void testAddLast() {
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        // first item should be 2
+        int removed = (int)deque.removeLast();
+        int expected = 4;
+        assertEquals(expected, removed);
+
+        removed = (int)deque.removeLast();
+        expected = 3;
+        assertEquals(expected, removed);
+
+        removed = (int)deque.removeLast();
+        expected = 1;
+        assertEquals(expected, removed);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddLast_ParameterNull_Exception() {
+        Deque deque = new Deque();
+        deque.addLast(null);
+    }
 
     @Test
     public void testRemoveFirst() {
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addFirst(2);
+        // first item should be 2
+        int removed = (int)deque.removeFirst();
+        int expected = 2;
+        assertEquals(expected, removed);
 
+        removed = (int)deque.removeFirst();
+        expected = 1;
+        assertEquals(expected, removed);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveFirst_DequeEmpty_Exception() {
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addFirst(1);
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
     }
 
     @Test
     public void testRemoveLast() {
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addFirst(2);
+        int last = (int) deque.removeLast();
 
+        assertEquals(1, last);
+        last = (int) deque.removeLast();
+        assertEquals(2, last);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testRemoveLast_DequeEmpty_Exception() {
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addFirst(2);
+        deque.removeFirst();
+        deque.removeLast();
+        deque.removeLast();
     }
 
     @Test
-    public void testGetIterator() {}
+    public void testGetIterator() {
+        Deque deque = new Deque();
+        deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(4);
+        deque.addLast(5);
+        Iterator iterator = deque.iterator();
+        int value  =  (int) iterator.next();
+        assertEquals(1, value);
+        value =  (int) iterator.next();
+        assertEquals(2, value);
+
+        value =  (int) iterator.next();
+        assertEquals(3, value);
+        value =  (int) iterator.next();
+        assertEquals(4, value);
+        value =  (int) iterator.next();
+        assertEquals(5, value);
+
+    }
+
+
+    @Test(expected = NoSuchElementException.class)
+    public void testNext_NoMoreElement_Exception() {
+        Deque deque = new Deque();
+        deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addFirst(1);
+        Iterator iterator = deque.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testNext_IteratorRemove_Exception() {
+        Deque deque = new Deque();
+        deque.addFirst(3);
+
+        Iterator iterator = deque.iterator();
+        iterator.remove();
+    }
 
 
 }
